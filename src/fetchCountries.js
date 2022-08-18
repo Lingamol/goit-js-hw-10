a0da7087d9a92cb7e3ede675cc13160b72d0f5c0
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-08-18 19:30:13 +0300

    2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,16 @@
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 export function fetchCountries(name) {
+  const searchParams = new URLSearchParams({
+    fields: ['name', 'capital', 'population', 'flags', 'languages'],
+  });
   Notiflix.Notify.info(`fetch:${name} `);
-  return fetch(`https://restcountries.com/v2/name/${name}`);
-  //   return fetch(` https://restcountries.com/v2/all`);
+  return fetch(
+    `https://restcountries.com/v2/name/${name}?${searchParams}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const DEBOUNCE_DELAY = 300;
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 const formData = {};
 const URL = 'https://restcountries.com/v2/';
+
 const refs = {
   inputForSearch: document.querySelector('#search-box'),
   countryList: document.querySelector('ul.country-list'),
@@ -18,24 +19,60 @@ refs.inputForSearch.addEventListener(
   debounce(handelInput, DEBOUNCE_DELAY)
 );
 function handelInput(e) {
+  refs.countryList.innerHTML = '';
   console.log(e.target.value.trim());
   if (e.target.value.trim() !== '') {
     fetchCountries(e.target.value.trim())
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
       .then(data => {
         if (data.length > 10) {
           Notiflix.Notify.info(
             'Too many matches found. Please enter a more specific name.'
           );
+        } else if (data.length > 1 && data.length < 10) {
+          renderCountrys(data);
+          console.log;
         } else {
-          console.log(data);
+          renderCountry(data);
+          console.log('only 1:', data);
         }
       })
-      .catch(error => console.log(error));
+      .catch(error => console.log('errorFetch', error));
   }
+}
+function renderCountrys(countrys) {
+  const markup = countrys
+    .map(({ name, capital, population, flags, languages }) => {
+      return `<li>
+      
+               
+          <h2 class="post-title"><img
+                  class="flags__img"
+                  srcset=""
+                  src=${flags.svg}
+                  alt="flags"
+                  width=100
+                  hight=100
+                />${name}</h2>
+          <p><b>Capital</b>: ${capital}</p>
+          <p><b>Population</b>: ${population}</p>
+          <p class="post-body">${languages.name}</p>
+        </li>`;
+    })
+    .join('');
+  refs.countryList.innerHTML = markup;
+}
+
+function renderCountry(country) {
+  console.log('country:', country);
+  const markup = country
+    .map(({ name, capital, population, flags, languages }) => {
+      return `<li>
+          <h2 class="-title">${name}</h2>
+          <p><b>Capital</b>: ${capital}</p>
+          <p><b>Population</b>: ${population}</p>
+          <p class="post-body">${languages.name}</p>
+        </li>`;
+    })
+    .join('');
+  refs.countryList.innerHTML = markup;
 }
